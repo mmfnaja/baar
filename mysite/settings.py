84482551e72976ff7dc5a04d7cb112c1f0d8773a
Nurulhuda <inanna.771@gmail.com>
2019-05-17 16:00:15 +0800

    account app

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'project.apps.ProjectConfig',
-    'project.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'actstream',
+    'account',
 ]
 
 MIDDLEWARE = [
